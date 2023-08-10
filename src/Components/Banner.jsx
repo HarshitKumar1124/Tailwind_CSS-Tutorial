8f48928bf909f83b369afaaa7d68ef2915395ecd
HarshitKumar1124 <harshitkr1124@gmail.com>
2023-08-10 23:49:54 +0530

    Banner & Expert Components Completed !!

---
+++ src/Components/Banner.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Typed from 'react-typed'
+
+const Banner = () => {
+  return (
+    <div className='bg-[#2699fb] w-full py-[100px] border border-black'>
+        <div className='max-w-[1240px] mx-auto text-center border font-bold border-[white]  mt-6'>
+
+            <div className='text-2xl md:text-3xl mt-4'>Learn With Us!</div>
+            <h1 className='text-white text-[50px] md:text-[70px] mt-4'>Grow with us.</h1>
+            <h2 className='text-[40px] md:text-[50px] mt-4'>Learn <Typed className='text-white' strings={['Web Developer','UX/UI Designer','Software Developer']} loop backSpeed={50} typeSpeed={60}/></h2>
+
+        </div>
+    </div>
+  )
+}
+
+export default Banner
